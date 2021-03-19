34563d3d24b50c65b8b17057b0b6b8274a3877c4
JosRigterink <rigterinkjos@gmail.com>
2021-03-19 16:09:47 +0100

    CrossBow en GunScript Toegevoegd

---
+++ Ontwikkeling Periode 3/Assets/Scripts/JosScripts/BorderScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BorderScript : MonoBehaviour
+{
+    public float range = 10f;
+    public GameObject canvas;
+    public RaycastHit hit;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+    public void BorderDetect()
+    {
+        if (Physics.Raycast(transform.position, transform.forward,out hit, range))
+        {
+            if (hit.transform.gameObject.tag == "WorldBorder")
+            {
+                canvas.SetActive(true);
+            }
+        }
+        
+    }
+    
+}
---
+++ Ontwikkeling Periode 3/Assets/Scripts/JosScripts/TargetScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetScript : MonoBehaviour
+{
+   public float health; 
+   public void TakeDamage (float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+}
