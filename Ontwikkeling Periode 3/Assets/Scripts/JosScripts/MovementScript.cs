57f78e8a25621e39db20156054062356c6a9b804
JosRigterink <rigterinkjos@gmail.com>
2021-03-26 20:59:22 +0100

    dingen aangepast aan mn level

--- Ontwikkeling Periode 3/Assets/Scripts/JosScripts/MovementScript.cs
+++ Ontwikkeling Periode 3/Assets/Scripts/JosScripts/MovementScript.cs
@@ -8,6 +8,8 @@ public class MovementScript : MonoBehaviour
     public float horizontal;
     public Vector3 move;
     public float speed;
+    public GameObject arrowPrefab;
+    public GameObject cam;
     
     // Update is called once per frame
     void Update()
@@ -19,7 +21,14 @@ public class MovementScript : MonoBehaviour
         move.z = vertical;
 
         GetComponent<Transform>().Translate(move * Time.deltaTime * speed);
-
+        
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject bulletObject = Instantiate(arrowPrefab);
+            bulletObject.transform.position = cam.transform.position + cam.transform.forward;
+            bulletObject.transform.forward = cam.transform.forward;
+        }
+       
 
     }
 
