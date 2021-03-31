9fbb26e83c0fdbf1c92471e1f6c034903144c2e3
JosRigterink <rigterinkjos@gmail.com>
2021-03-31 16:47:25 +0200

    portals werkend gemaakt en particles beter gemaakt

---
+++ Ontwikkeling Periode 3/Assets/Scripts/JosScripts/PortalScript3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PortalScript3 : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            SceneManager.LoadScene("Level Toekomst");
+        }
+
+    }
+}
