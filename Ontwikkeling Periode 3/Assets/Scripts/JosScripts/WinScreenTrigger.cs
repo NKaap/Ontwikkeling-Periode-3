f4ea209be4c6e3d552bbdbe1547c600f2b2217e1
JosRigterink <rigterinkjos@gmail.com>
2021-04-01 14:05:32 +0200

    dingen verandert aan mn level

---
+++ Ontwikkeling Periode 3/Assets/Scripts/JosScripts/WinScreenTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class WinScreenTrigger : MonoBehaviour
+{
+    public GameObject winScreen;
+    private void Start()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Cursor.lockState = CursorLockMode.None;
+            winScreen.SetActive(true);
+        }
+    }
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene ("levelVerledenJos");
+    }
+
+
+}
