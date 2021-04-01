5413b6ef4666faaf716aab9bf1e16a28d9ab5559
Sjoerd21 <78026651+Sjoerd21@users.noreply.github.com>
2021-04-01 09:45:28 +0200

    ontikkeling sjoerd

--- Ontwikkeling Periode 3/Assets/Scripts/Noah/Shooting.cs
+++ Ontwikkeling Periode 3/Assets/Scripts/Noah/Shooting.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+<<<<<<< HEAD
+=======
+    public float damage;
+    public float range;
+    public RaycastHit hit;
+>>>>>>> 0c211ff0fff0aa78c29c315bdacf46851bba5c8c
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,19 @@ public class Shooting : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+<<<<<<< HEAD
         
+=======
+        if (Input.GetButton("Fire1"))
+        {
+            if(Physics.Raycast(transform.position, transform.forward, out hit, range))
+            {
+                if(hit.collider.tag == "Enemy")
+                {
+                    
+                }
+            }
+        }
+>>>>>>> 0c211ff0fff0aa78c29c315bdacf46851bba5c8c
     }
 }
